c60879215847aa764dec1558738780456867ef15
ashleyborum <ashb4101@gmail.com>
2023-01-31 17:54:12 -0500

    Added: sample test

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div data-testid="test-1" className="App">
       <header className="App-header">
         <p>
           Welcome to World Space!
--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,17 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, cleanup } from '@testing-library/react';
 import App from './App';
 
-// Sample test using Jest
+// React app tests using Jest
+
+// Sample test 0 - passed
 test('test', () => {
   expect(true).toBe(true);
+})
+
+// Sample test 1 - passed
+test('test1', () => {
+  render(<App/>)
+  const testElement = screen.getByTestId('test-1');
+  expect(testElement).toBeInTheDocument();
+  expect(testElement).toHaveTextContent('Welcome');
 })

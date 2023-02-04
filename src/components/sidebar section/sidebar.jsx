25dc78b2a33ba019631d5446365d0070d7ffec21
ashleyborum <ashb4101@gmail.com>
2023-02-04 17:17:08 -0500

    sidebar creation

--- src/components/sidebar section/sidebar.jsx
+++ src/components/sidebar section/sidebar.jsx
@@ -1,9 +1,25 @@
 import React from "react";
 import './sidebar.css';
 
+// Imported Images
+import logo from '../../Assets/logo.png';
+
+// placeholder logo image
 const Sidebar = () => {
     return (
-        <div>Sidebar</div>
+        <div className="sideBar grid">
+            <div className="logoDiv flex">
+                <img src={logo} alt="World Space Logo" /> 
+                <h2>World Space</h2>
+            </div>
+
+            <div className="menuDiv">
+                <h3 className="divTitle">
+                    QUICK MENU
+                </h3>
+
+            </div>
+        </div>
     );
 }
 

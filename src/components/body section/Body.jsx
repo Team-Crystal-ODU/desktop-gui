3d8273ea16fd743252b87fd9917d65b5b9c9a24f
ashleyborum <ashb4101@gmail.com>
2023-02-05 09:26:05 -0500

    Added: sidebar styles

--- src/components/body section/Body.jsx
+++ src/components/body section/Body.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 import './body.css';
+import Top from './top section/Top';
+import Listing from './listing section/Listing';
+import Activity from './activity section/Activity';
 
 const Body = () => {
     return (
-        <div>Body</div>
+        <div className="mainContent">
+            <Top />
+
+            <div className="bottom flex">
+                <Listing />
+                <Activity />
+            </div>
+        </div>
     );
 }
 

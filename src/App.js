1a53616fb3cc51c59873ad36f275740ff5773976
ashleyborum <ashb4101@gmail.com>
2023-02-09 08:05:41 -0500

    Deleted: excess code

--- src/App.js
+++ src/App.js
@@ -2,18 +2,6 @@ import './App.css';
 import Sidebar from './components/sidebar section/Sidebar';
 import Body from './components/body section/Body';
 
-/* function App() {
-  return (
-    <div data-testid="test-1" className="App">
-      <header className="App-header">
-        <p>
-          Welcome to World Space!
-        </p>
-      </header>
-    </div>
-  );
-} */
-
 function App() {
   return (
     <div data-testid="test-1" className="container">

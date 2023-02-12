100d0f8a2d3c5619778336764117c88e82370173
Mtan006 <mtan006@odu.edu>
2023-02-12 03:17:37 -0500

    update code

---
+++ src/components/news section/NewsContext.js
@@ -0,0 +1,24 @@
+import React, { createContext, useEffect, useState } from "react";
+import axios from "axios";
+
+export const NewsContext = createContext();
+
+export const NewsContextProvider = (props) => {
+  const [data, setData] = useState();
+  const apiKey = "8b3857ce9ec64963a71559484c640e1b";
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://newsapi.org/v2/everything?q=climatechange&from=2023-01-11&sortBy=publishedAt&apiKey=${apiKey}`
+      )
+      .then((response) => setData(response.data))
+      .catch((error) => console.log(error));
+  }, []);
+
+  return (
+    <NewsContext.Provider value={{ data }}>
+      {props.children}
+    </NewsContext.Provider>
+  );
+};
---
+++ src/components/news section/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import News from "./News";
+
+ReactDOM.render(<News />, document.getElementById("root"));

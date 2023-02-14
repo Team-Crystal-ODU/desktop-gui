22847dba99eef83606af7b3a6f60fe5b1e4a0d57
Mtan006 <mtan006@odu.edu>
2023-02-13 20:51:02 -0500

    update eco pics size

--- src/components/news section/components/Newsart.js
+++ src/components/news section/components/Newsart.js
@@ -5,7 +5,7 @@ function Newsart({ data }) {
     <div className="news">
       <h1 className="news__title">{data.title}</h1>
       <h1 className="news__url">{data.url}</h1>
-      <img src={data.urlToImage} alt="world space"/>
+      <img className = "news__images" src={data.urlToImage} alt="world space"/>
       <p className="news__desc">{data.description}</p>
       <span className="news__author">{data.author}</span> <br />
       <span className="news__published">{data.publishedAt}</span>

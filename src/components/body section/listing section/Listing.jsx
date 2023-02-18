95c443ac79d95696b2ca276a66520c1d120b7777
ashleyborum <ashb4101@gmail.com>
2023-02-17 20:24:30 -0500

    Added: dashboard content and styles

--- src/components/body section/listing section/Listing.jsx
+++ src/components/body section/listing section/Listing.jsx
@@ -1,9 +1,38 @@
 import React from "react";
 import './listing.css';
 
+// Imported Images ============>
+import img from '../../../Assets/globe(1).png';
+
 const Listing = () => {
     return (
-        <div>Listing</div>
+        <div className="listingSection">
+            <div className="rankSection">
+                <div className="innerRank"></div>
+            </div>
+
+            <div className="secContainer">
+                <div className="secTitle">Consumption Overview</div>
+                <div className="singleItem">
+                    <span className="score">50</span>
+                    <h3>Readout</h3>
+                </div>
+
+                <div className="singleItem">
+                    <span className="score">43</span>
+                    <h3>Driven Equivalent</h3>
+                </div>
+
+                <div className="singleItem efficiency">
+                    <span className="score">33</span>
+                    <h3 className="efficiencyScore">Your Efficiency Score</h3>
+                </div>
+            </div>
+
+            <div className="ecosystem flex">
+                <img src={img} alt="Image Title" />
+            </div>
+        </div>
     );
 }
 

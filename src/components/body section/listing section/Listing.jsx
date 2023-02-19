a91a44ad27b1aa808ba85c03d317d822dc80efc6
ashleyborum <ashb4101@gmail.com>
2023-02-19 00:11:11 -0500

    Added: dashboard content and styles

--- src/components/body section/listing section/Listing.jsx
+++ src/components/body section/listing section/Listing.jsx
@@ -3,34 +3,44 @@ import './listing.css';
 
 // Imported Images ============>
 import img from '../../../Assets/globe(1).png';
+import img2 from '../../../Assets/graph.png';
 
 const Listing = () => {
     return (
         <div className="listingSection">
-            <div className="rankSection">
+            
+            <div className="rank">
                 <div className="innerRank"></div>
             </div>
 
-            <div className="secContainer">
-                <div className="secTitle">Consumption Overview</div>
-                <div className="singleItem">
-                    <span className="score">50</span>
-                    <h3>Readout</h3>
+            <div className="overviewSection flex"> 
+             
+                <div className="leftOverview">
+                    <div className="title">Consumption Overview</div>
+                    <div className="singleItem">
+                        <span className="score">50</span>
+                        <h3>Readout</h3>
+                    </div>
+
+                    <div className="singleItem">
+                        <span className="score">43</span>
+                        <h3>Driven Equivalent</h3>
+                    </div>
+
+                    <div className="singleItem efficiency">
+                        <span className="score">33</span>
+                        <h3 className="efficiencyScore">Your Efficiency Score</h3>
+                    </div>
                 </div>
 
-                <div className="singleItem">
-                    <span className="score">43</span>
-                    <h3>Driven Equivalent</h3>
+                <div className="middleOverview flex">
+                    <img src={img} alt="Image Title" /> 
                 </div>
 
-                <div className="singleItem efficiency">
-                    <span className="score">33</span>
-                    <h3 className="efficiencyScore">Your Efficiency Score</h3>
+                <div className="rightOverview flex">
+                    <img src={img2} alt="Image Title" />
                 </div>
-            </div>
 
-            <div className="ecosystem flex">
-                <img src={img} alt="Image Title" />
             </div>
         </div>
     );

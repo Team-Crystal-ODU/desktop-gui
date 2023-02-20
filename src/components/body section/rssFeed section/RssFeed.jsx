05cc375b78b9d209fda3d29d07532ebd7dcecebe
ashleyborum <ashb4101@gmail.com>
2023-02-20 17:15:44 -0500

    Added: RSS feed section and styles

---
+++ src/components/body section/rssFeed section/RssFeed.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './rssFeed.css';
+
+
+const RssFeed = () => {
+    return (
+        <div className="rssFeedSection">
+            <div className="title">
+                <a href="/news">News Page (click me)</a>
+            </div>
+        </div>
+    );
+}
+
+export default RssFeed;

e291a2523433566e08cb9dce12942253281e9ead
ashleyborum <ashb4101@gmail.com>
2023-02-28 14:41:20 -0500

    Added: RSS feed directories

---
+++ rss-server/index.js
@@ -0,0 +1,31 @@
+import cors from "cors";
+import express from "express";
+import RSSParser from "rss-parser";
+
+const feedURL = "https://yaleclimateconnections.org/feed/";
+const parser = new RSSParser();
+let articles = [];
+const parse = async url => {
+    const feed = await parser.parseURL(url);
+
+    //console.log(feed.title);
+
+    feed.items.forEach(item => {
+        //console.log(`${item.title}\n${item.link}\n\n`);
+        articles.push({ item })
+    })
+}
+parse(feedURL);
+
+let app = express();
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send(articles);
+})
+
+const server = app.listen("4000", () => {
+    console.log("App is listening at http://localhost:4000");
+})
+
+export default server;

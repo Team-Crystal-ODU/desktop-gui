3560cc3a7a590597ed97a84efc866e58c48d65b4
ashleyborum <ashb4101@gmail.com>
2023-03-01 05:15:37 -0500

    Update: page routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
-import Sidebar from './components/sidebar section/Sidebar';
+//import Sidebar from './components/sidebar section/Sidebar';
 import Body from './components/body section/Body';
 import News from './components/news section/News';
+import Login from './components/login section/Login';
+import Register from "./components/login section/Register";
 
 import { Routes, Route } from 'react-router-dom';
 
@@ -10,7 +12,9 @@ function App() {
     <div data-testid="test-1" className="container">
       <Routes>
         {/*<Sidebar />*/}
-        <Route path='/' element={<Body />} />
+        <Route path='/' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/home' element={<Body />} />
         <Route path='/news' element={<News />} />
       </Routes>
     </div>

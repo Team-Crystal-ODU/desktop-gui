c9463090fb0ac84db7bd7fab05d5c42505981963
ashleyborum <ashb4101@gmail.com>
2023-03-07 13:07:55 -0500

    Increased electron window size

--- public/electron.js
+++ public/electron.js
@@ -6,8 +6,8 @@ const { app, BrowserWindow } = require('electron');
 function createWindow() {
   // Create the browser window.
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1200,
+    height: 800,
     webPreferences: {
       nodeIntegration: true,
     },

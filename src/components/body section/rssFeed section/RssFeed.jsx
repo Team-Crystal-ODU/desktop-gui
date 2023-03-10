dadb6ce1388f9f0f0903ffd1706746f45d3600bf
ashleyborum <ashb4101@gmail.com>
2023-03-10 13:25:41 -0500

    Reinstated RssFeed on dashboard

--- src/components/body section/rssFeed section/RssFeed.jsx
+++ src/components/body section/rssFeed section/RssFeed.jsx
@@ -1,13 +1,28 @@
 import React from "react";
 import './rssFeed.css';
 import App from "./rss/src/App"
+
+
 const RssFeed = ({ title, date }) => {
     return (
         <div className="rssFeedSection">
-            <App />
-        </div>
-             
+            <div className="overviewSection">
+                
+                <div className="leftOverview">
+                    <div className="singleItem">
+                        <div className="title">
+                            <h3 className="text-xl mb-1">{title}</h3>
+                        </div>
+                        <p className="date">{date}</p>
+                    </div>
+                </div>
+            </div>
+        </div>  
     )
 }
 
 export default RssFeed;
+
+{/* <div className="rssFeedSection">
+        <App />
+</div> */}

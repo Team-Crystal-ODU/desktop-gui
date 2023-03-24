ebee210a3bc1ad2f5f06eeeb77d4d2bca8cae177
ashleyborum <ashb4101@gmail.com>
2023-03-24 16:45:00 -0400

    Fix: page formatting

--- src/components/body section/rssFeed section/rss/src/App.js
+++ src/components/body section/rssFeed section/rss/src/App.js
@@ -22,23 +22,25 @@ function App() {
   console.log(articles);
   return (
     <>
-  <div className="head__text">
-        <h1>Eco News</h1>
-        <a href="/">Return to Home</a>
-  </div>
-<div className="borders">
-  <div className="pics">
-    <img src = "https://i0.wp.com/yaleclimateconnections.org/wp-content/uploads/2021/03/YCC-Logo-website-1.png?resize=2048%2C612&ssl=1" />
-  </div>
-    {articles.map((item, i) =>
-    <Feed 
-    key = {i}
-    title = {item.item.title}
-    link = {item.item.link}
-    date = {item.item.pubDate}
-    />
-    )}
-  </div>
+      <div className="newsContainer">
+        <div className="head__text">
+              <h1>Eco News</h1>
+              <a href="/">Return to Home</a>
+        </div>
+        <div className="articles">
+            <div className="pics">
+              <img src = "https://i0.wp.com/yaleclimateconnections.org/wp-content/uploads/2021/03/YCC-Logo-website-1.png?resize=2048%2C612&ssl=1" />
+            </div>
+            {articles.map((item, i) =>
+            <Feed 
+              key = {i}
+              title = {item.item.title}
+              link = {item.item.link}
+              date = {item.item.pubDate}
+            />
+            )}
+        </div>
+      </div>
     </>
   );
 }

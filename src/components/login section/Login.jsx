5b9ab2b7e2b4b965216bca8c73c68ed2746a77bf
ashleyborum <ashb4101@gmail.com>
2023-04-08 16:39:28 -0400

    Added: login restrictions

--- src/components/login section/Login.jsx
+++ src/components/login section/Login.jsx
@@ -2,30 +2,54 @@ import React, { useRef, useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import './login.css';
 
+const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
+const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
+
 export const Login = () => {
     const userRef = useRef();
     const errRef = useRef();
 
     const [user, setUser] = useState('');
+    const [validName, setValidName] = useState(false);
+    const [userFocus, setUserFocus] = useState(false);
+
     const [pwd, setPwd] = useState('');
+    const [validPwd, setValidPwd] = useState(false);
+    const [pwdFocus, setPwdFocus] = useState(false);
 
     const [errMsg, setErrMsg] = useState('');
     const [success, setSuccess] = useState(false);
 
     const navigate = useNavigate();
 
-    /*useEffect(() => {
+    useEffect(() => {
         userRef.current.focus();
-    }, [])*/
+    }, [])
+
+    //test to confirm name is valid according to restrictions
+    useEffect(() => {
+        setValidName(USER_REGEX.test(user));
+    }, [user])
+
+    //test to confirm password is valid according to restrictions
+    useEffect(() => {
+        setValidPwd(PWD_REGEX.test(pwd));
+    }, [pwd])
 
     useEffect(() => {
         setErrMsg('');
     }, [user, pwd])
 
     const handleSubmit = async (e) => {
-        /* Temp Login Success */
         e.preventDefault();
-        console.log(user, pwd);
+        const v1 = USER_REGEX.test(user);
+        const v2 = PWD_REGEX.test(pwd);
+        if (!v1 || !v2) {
+            setErrMsg("Invalid Entry");
+            return;
+        }
+
+        /* Temp Login Success */
         setUser('');
         setPwd('');
         setSuccess(true);
@@ -48,22 +72,49 @@ export const Login = () => {
                 <h2>Login</h2>
                 <form className="login-form" onSubmit={handleSubmit}>
                     <label htmlFor="username">username</label>
-                    <input value={user} 
-                        onChange={(e) => setUser(e.target.value)}
-                        type="text" 
-                        placeholder="username" 
+                    <input 
+                        type="text"
                         id="username" 
+                        ref={userRef}
                         autoComplete="off"
-                        required />
+                        onChange={(e) => setUser(e.target.value)}
+                        value={user}  
+                        required
+                        placeholder="username" 
+                        aria-invalid={validName ? "false" : "true"}
+                        aria-describedby="uidnote"
+                        onFocus={() => setUserFocus(true)}
+                        onBlur={() => setUserFocus(false)}  
+                    />
+                    {/* Accessibility Note */}
+                    <p id="uidnote" className={userFocus && user && !validName ? "instructions" : "offscreen"}>
+                        4 to 24 characters.<br />
+                        Must begin with a letter.<br />
+                        Letters, numbers, underscores, hyphens allowed.
+                    </p>
+
                     <label htmlFor="password">password</label>
-                    <input value={pwd} 
-                        onChange={(e) => setPwd(e.target.value)} 
+                    <input
                         type="password" 
-                        placeholder="********" 
-                        id="password" 
-                        name="password" 
-                        required />
+                        id="password"
+                        onChange={(e) => setPwd(e.target.value)}
+                        value={pwd}  
+                        required 
+                        aria-invalid={validPwd ? "false" : "true"}
+                        aria-describedby="pwdnote"
+                        onFocus={() => setPwdFocus(true)}
+                        onBlur={() => setPwdFocus(false)} 
+                        placeholder="********"  
+                        name="password"  
+                    />
+                    <p id="pwdnote" className={pwdFocus && !validPwd ? "instructions" : "offscreen"}>
+                        8 to 24 characters.<br />
+                        Must include uppercase and lowercase letters, a number and a special character.<br />
+                        Allowed special characters: <span aria-label="exclamation mark">!</span> <span aria-label="at symbol">@</span> <span aria-label="hashtag">#</span> <span aria-label="dollar sign">$</span> <span aria-label="percent">%</span>
+                    </p>
+
                     <button type="submit">Log In</button>
+                    {/*<button disabled={!validName || !validPwd ? true : false}>Log In</button>*/}
                 </form>
                 <a href="/register" className="link-btn">Don't have an account? Register here.</a>
             </div>
